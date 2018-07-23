caf791a983489308487aed07224473e209791267
Student <example@example.com>
2018-07-23 15:48:51 -0700

    silly map flexbox

--- POS/wwwroot/js/site.js
+++ POS/wwwroot/js/site.js
@@ -1,16 +1,1 @@
 ﻿
-
-function book_suggestion()
-{
-var food = document.getElementById("food").value;
-var xhr;
- if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-    xhr = new XMLHttpRequest();
-} else if (window.ActiveXObject) { // IE 8 and older
-    xhr = new ActiveXObject("Microsoft.XMLHTTP");
-}
-var data = "name=" + food;
-     xhr.open("POST", "book-suggestion.php", true); 
-     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");                  
-     xhr.send(data);
-}
