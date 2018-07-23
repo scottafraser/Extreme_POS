dc08440f2c66b98cb6bebcb26f5fccd8beab1429
Student <example@example.com>
2018-07-23 14:47:58 -0700

    build viewmodel

--- POS/wwwroot/js/site.js
+++ POS/wwwroot/js/site.js
@@ -1,1 +1,16 @@
-﻿// Write your Javascript code.
+﻿
+
+function book_suggestion()
+{
+var food = document.getElementById("food").value;
+var xhr;
+ if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+    xhr = new XMLHttpRequest();
+} else if (window.ActiveXObject) { // IE 8 and older
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+}
+var data = "name=" + book;
+     xhr.open("POST", "book-suggestion.php", true); 
+     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");                  
+     xhr.send(data);
+}
