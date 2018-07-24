247399d576687739d11a54e86f88c8b2a478b997
Student <example@example.com>
2018-07-24 10:56:51 -0700

    fix up tabbing stylature, and replace ajax test from  .append to .html

--- POS/wwwroot/js/scripts.js
+++ POS/wwwroot/js/scripts.js
@@ -1,4 +1,4 @@
-var menuNav = function() {
+var menuDisplay = function() {
     $(".menu button").click(function() {
         var target = $(this).attr("id");
         $(".display").hide();
@@ -6,17 +6,31 @@ var menuNav = function() {
     });
 }
 
-$(function() {
-    menuNav();
+var menuNav = function() {
+    $(".menu button").click(function() {
+        $(this).parent().siblings().removeClass("selected");
+        $(this).parent().addClass("selected");
+    });
+}
 
+var testAjax = function() {
     $('.menu button').click(function () {
       $.ajax({
         type: 'GET',
         url: '/Mockup/TestPull',
         success: function (result) {
-          $('#food-display').append(result);
+          $('#food-display').html(result);
         }
       });
     });
+}
+
+var menu
+
+$(function() {
+    menuNav();
+    menuDisplay();
+    testAjax();
+
 
 });
