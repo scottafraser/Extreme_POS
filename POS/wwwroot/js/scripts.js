af4d7b67541a36f2b04e7cf5cd5ae50e3663de26
Student <example@example.com>
2018-07-24 09:17:12 -0700

    add button functionality for menu displays

--- POS/wwwroot/js/scripts.js
+++ POS/wwwroot/js/scripts.js
@@ -1,3 +1,11 @@
+var menuNav = function() {
+    $(".menu button").click(function() {
+        var target = $(this).attr("id");
+        $(".display").hide();
+        $("#" + target + "-display").show();
+    });
+}
+
 $(function() {
-   
-})
+    menuNav();
+});
